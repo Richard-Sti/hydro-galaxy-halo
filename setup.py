b9ee8aba56ad8e329d8c2879a0c582d0544a73fe
Richard <richard.stiskalek@protonmail.com>
2021-04-04 09:58:53 +0200

    More pep8

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',],
+        'Programming Language :: Python :: 3.8']
 )
